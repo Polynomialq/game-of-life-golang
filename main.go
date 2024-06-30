b6fdd57d9bed0752f0c8ac772dfa2cce9eb6d076
Михаил Лукашов <91984345+Polynomialq@users.noreply.github.com>
2024-06-30 16:10:27 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"image/color"
+	"log"
+	"math/rand"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const scale int = 2
+const height = 720
+const width = 1080
+
+var purple color.Color = color.RGBA{128, 0, 128, 255}
+var yellow color.Color = color.RGBA{255, 230, 120, 255}
+var grid [width][height]uint8 = [width][height]uint8{}
+var buffer [width][height]uint8 = [width][height]uint8{}
+var count int = 0
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	for x := 1; x < width-1; x++ {
+		for y := 1; y < height-1; y++ {
+			buffer[x][y] = 0
+			n := grid[x-1][y-1] + grid[x-1][y+0] + grid[x-1][y+1] + grid[x+0][y-1] + grid[x+0][y+1] + grid[x+1][y-1] + grid[x+1][y+0] + grid[x+1][y+1]
+
+			if grid[x][y] == 0 && n == 3 {
+				buffer[x][y] = 1
+			} else if n < 2 || n > 3 {
+				buffer[x][y] = 0
+			} else {
+				buffer[x][y] = grid[x][y]
+			}
+		}
+	}
+
+	temp := buffer
+	buffer = grid
+	grid = temp
+	return nil
+}
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(purple)
+
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			for i := 0; i < scale; i++ {
+				for j := 0; j < scale; j++ {
+					if grid[x][y] == 1 {
+						screen.Set(x*scale+i, y*scale+j, yellow)
+					}
+				}
+			}
+		}
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return width, height
+}
+
+func main() {
+	for x := 1; x < width-1; x++ {
+		for y := 1; y < height-1; y++ {
+			if rand.Float32() < 0.5 {
+				grid[x][y] = 1
+			}
+		}
+	}
+	ebiten.SetWindowSize(width, height)
+	ebiten.SetWindowTitle("Game Of Life")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
